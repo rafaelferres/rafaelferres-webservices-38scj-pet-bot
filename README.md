66b5bdd4f739ebc12ab034f87cf242b136e0f03e
Rafael Ferrés <raferres@outlook.com.br>
2021-05-11 00:22:31 -0300

    Add express

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Serviço que insere dados na api do pets
 
 Para rodar é só rodar o comando npm install && npm start
+
+## Consumir api
+
+Enviar post para /addBpm com o payload { bpm : number } para fazer o envio de um bpm
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const express = require('express')
 
 const loopInsertBpm = async () => {
     const user = {
@@ -42,5 +43,55 @@ const loopInsertBpm = async () => {
 
       console.log(getBpm.data)
 }
+
+const loopInsertBpmApi = async (req, res) => {
+  const user = {
+      "email": "teste@teste.com.br",
+      "password": "abc123"
+  }
+
+  const loginResponse = await axios({
+      method: 'post',
+      url: 'https://webservices-pets-api.herokuapp.com/api/v1/user/login',
+      data: user
+    });
+
+  const token = loginResponse.data.token
+
+  const getPetsResponse = await axios({
+      method: 'get',
+      url: 'https://webservices-pets-api.herokuapp.com/api/v1/pets',
+      headers: { Authorization: `Bearer ${token}` }
+    }); 
+
+  const pet = getPetsResponse.data[0].id
+  const addBpm = await axios({
+      method: 'post',
+      url: 'https://webservices-pets-api.herokuapp.com/api/v1/bpm',
+      headers: { Authorization: `Bearer ${token}` },
+      data: {
+          "pet": pet,
+          "bpm": req.body.bpm
+      }
+    }); 
+
+    const getBpm = await axios({
+      method: 'get',
+      url: 'https://webservices-pets-api.herokuapp.com/api/v1/bpm',
+      headers: { Authorization: `Bearer ${token}` },
+      data: {
+          "pet": pet
+      }
+    }); 
+
+    res.json(getBpm.data)
+}
+
+const app = express()
+app.use(express.json())
+
+app.post('/addBpm', (req, res) => loopInsertBpmApi(req, res))
+
+app.listen(process.env.PORT || 8080)
 
 setInterval(() => { loopInsertBpm() },1000)
